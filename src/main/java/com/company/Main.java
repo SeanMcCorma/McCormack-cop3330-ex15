2c451d49691cfacc1ebb93180ba42abf934bc57b
Seanfrogs <55803970+Seanfrogs@users.noreply.github.com>
2021-09-11 01:00:35 -0400

    Initial commit

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("What is the password? ");
+        String input = scan.nextLine();
+        String password = "abc$123";
+        if(password.equals(input)){
+            System.out.println("Welcome!");
+        }else{
+            System.out.println("I don't know you.");
+        }
+	// write your code here
+    }
+}
